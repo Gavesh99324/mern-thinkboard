7db00ca4fe7427d74d286c0ced857f508f933233
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-12-02 16:48:37 +0530

    RA-396 Check PR relation with issues (Fixes #32)

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -48,7 +48,7 @@ export async function updateNote(req, res) {
       return res.status(404).json({ message: "Note not found" });
     res.status(200).json({ message: "Note updated successfully" });
   } catch (error) {
-    console.error("bugs in updateNotes controllers", error);
+    console.error("error and bugs in updateNotes controllers", error);
     res.status(500).json({ message: "Internal server error" });
   }
 }
